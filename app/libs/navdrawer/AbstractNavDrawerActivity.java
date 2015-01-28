d2287f94c539b86cd4b46dc572ff1b0b3403589e
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-01-29 00:58:26 +0530

    Initial Commit

--- app/libs/navdrawer/AbstractNavDrawerActivity.java
+++ app/libs/navdrawer/AbstractNavDrawerActivity.java
@@ -14,7 +14,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import org.michenux.drodrolib.MCXApplication;
+import libs.applib.TDSApplication;
 
 public abstract class AbstractNavDrawerActivity extends ActionBarActivity {
 
