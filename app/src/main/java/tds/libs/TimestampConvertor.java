a5d90c5c896db2645ac63a400cc723c1f9f1765e
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-01-29 20:34:08 +0530

    Initial Commit

---
+++ app/src/main/java/tds/libs/TimestampConvertor.java
@@ -0,0 +1,61 @@
+package tds.libs;
+
+/**
+ * Created by laks on 29-01-2015.
+ */
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+public class TimestampConvertor {
+
+    public String usingDateAndCalendar(long input){
+        Date date = new Date(input);
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(date);
+        return(cal.get(Calendar.YEAR)
+                + "/" + cal.get(Calendar.MONTH)
+                + "/" + cal.get(Calendar.DATE)
+                + " " + cal.get(Calendar.HOUR)
+                + ":" + cal.get(Calendar.MINUTE)
+                + ":" + cal.get(Calendar.SECOND)
+                + (cal.get(Calendar.AM_PM)==0?"AM":"PM")
+        );
+
+    }
+
+    public String usingDateAndCalendarWithTimeZone(long input){
+        Date date = new Date(input);
+        Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT+5:30"));
+        cal.setTime(date);
+        return(cal.get(Calendar.YEAR)
+                + "/" + cal.get(Calendar.MONTH)
+                + "/" + cal.get(Calendar.DATE)
+                + " " + cal.get(Calendar.HOUR)
+                + ":" + cal.get(Calendar.MINUTE)
+                + ":" + cal.get(Calendar.SECOND)
+                + (cal.get(Calendar.AM_PM)==0?"AM":"PM")
+        );
+
+    }
+    public String usingDateFormatter(long input){
+        Date date = new Date(input);
+        Calendar cal = new GregorianCalendar();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MMM/dd hh:mm:ss z");
+        sdf.setCalendar(cal);
+        cal.setTime(date);
+        return sdf.format(date);
+    }
+
+    public String usingDateFormatterWithTimeZone(long input){
+        Date date = new Date(input);
+        Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MMM/dd hh:mm:ss z");
+        sdf.setCalendar(cal);
+        cal.setTime(date);
+        return sdf.format(date);
+    }
+}
--- app/src/main/java/tds/socio/AttendanceActivity.java
+++ app/src/main/java/tds/socio/AttendanceActivity.java
@@ -5,15 +5,21 @@ package tds.socio;
  */
 
 import android.content.res.TypedArray;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import tds.libs.GPSTracker;
+import tds.libs.SntpClient;
+import tds.libs.TimestampConvertor;
 
 public class AttendanceActivity extends BaseActivity {
     private String[] navMenuTitles;
     private TypedArray navMenuIcons;
+    long lngCurrentTime = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +31,28 @@ public class AttendanceActivity extends BaseActivity {
                 .obtainTypedArray(R.array.nav_drawer_icons);
 
         set(navMenuTitles, navMenuIcons);
+
+        new RetrieveTime().execute();
+
     }
 
-    public void GetLocation(View view)
+    public void MarkInAttendance(View view)
     {
-        GPSTracker gps = new GPSTracker(AttendanceActivity.this);
-        if(gps.canGetLocation()){
 
+        GPSTracker gps = new GPSTracker(AttendanceActivity.this);
+
+        if(gps.canGetLocation())
+        {
             double latitude = gps.getLatitude();
             double longitude = gps.getLongitude();
-
 //            EditText editTextlat = (EditText)findViewById(R.id.latitude);
 //            editTextlat.setText(String.valueOf(latitude));
 //            EditText editTextlong = (EditText)findViewById(R.id.longitude);
 //            editTextlong.setText(String.valueOf(longitude));
 //            EditText editDistance = (EditText)findViewById(R.id.distance);
 //            editDistance.setText(StrdistBetCoor);
-
             String StrdistBetCoor = Double.toString(gps.distance(latitude, longitude, 17.3986852, 78.3896163, 'C'));
             Toast.makeText(getApplicationContext(),StrdistBetCoor,Toast.LENGTH_LONG).show();
-
         }
         else
         {
@@ -52,5 +60,26 @@ public class AttendanceActivity extends BaseActivity {
         }
     }
 
-}
+    class RetrieveTime extends AsyncTask<Void, Void, Void>
+    {
+        protected Void doInBackground(Void... params) {
 
+            try {
+                SntpClient currentTime = new SntpClient();
+
+                if (currentTime.requestTime("2.in.pool.ntp.org", 60000)) {
+                    lngCurrentTime = currentTime.getNtpTime();
+
+                    TextView TVcurrentTime = (TextView) findViewById(R.id.DateTimeNow);
+                    java.util.Date d = new java.util.Date(lngCurrentTime);
+
+                    TimestampConvertor tsc = new TimestampConvertor();
+                    TVcurrentTime.setText(tsc.usingDateAndCalendarWithTimeZone(lngCurrentTime));
+                }
+            } catch (Exception e) {
+                Log.e("Async Task - ", "Exception Raised: " + e);
+            }
+            return null;
+        }
+    }
+}
