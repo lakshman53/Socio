3cd06cff9cdcf415c86e9b0eabe3689b365ec65b
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-01-29 12:14:16 +0530

    Initial Commit

--- app/src/main/java/tds/socio/GeneralActivity.java
+++ app/src/main/java/tds/socio/GeneralActivity.java
@@ -14,7 +14,6 @@ public class GeneralActivity extends ActionBarActivity {
         setContentView(R.layout.activity_general);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
