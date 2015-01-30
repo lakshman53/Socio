a7a1761717c713461ea723a57c7e4a6d8c29e01d
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-01-30 09:32:08 +0530

    Initial Commit

--- app/src/main/java/tds/libs/db/dsl/Table.java
+++
@@ -1,9 +0,0 @@
-package tds.libs.db.dsl;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Table {
-    String name() default "";
-}
