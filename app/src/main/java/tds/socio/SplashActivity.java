9145e4734a60c2213197024e991de8a4b9077f83
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-01-30 11:14:07 +0530

    Initial Commit

--- app/src/main/java/tds/socio/SplashActivity.java
+++ app/src/main/java/tds/socio/SplashActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-
 public class SplashActivity extends Activity {
 
     /** Duration of wait **/
@@ -23,7 +22,7 @@ public class SplashActivity extends Activity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity.*/
-                Intent mainIntent = new Intent(SplashActivity.this, AttendanceActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
