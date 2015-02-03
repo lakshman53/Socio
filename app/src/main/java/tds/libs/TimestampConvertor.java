6aa86ffaa9bb32547723ac904d5eca11f9a8e041
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-02-03 19:10:03 +0530

    Initial Commit

--- app/src/main/java/tds/libs/TimestampConvertor.java
+++ app/src/main/java/tds/libs/TimestampConvertor.java
@@ -4,6 +4,7 @@ package tds.libs;
  * Created by laks on 29-01-2015.
  */
 
+import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -31,16 +32,30 @@ public class TimestampConvertor {
         Date date = new Date(input);
         Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT+5:30"));
         cal.setTime(date);
-        return(cal.get(Calendar.DATE)
-                + "/" + cal.get(Calendar.MONTH)
+        return(addLeadingZero(cal.get(Calendar.DATE))
+                + "/" + getMonthForInt(cal.get(Calendar.MONTH))
                 + "/" + cal.get(Calendar.YEAR)
-                + " " + cal.get(Calendar.HOUR)
-                + ":" + cal.get(Calendar.MINUTE)
-                + ":" + cal.get(Calendar.SECOND)
-                + (cal.get(Calendar.AM_PM)==0?"AM":"PM")
+                + " " + addLeadingZero(cal.get(Calendar.HOUR))
+                + ":" + addLeadingZero(cal.get(Calendar.MINUTE))
+//                + ":" + addLeadingZero(cal.get(Calendar.SECOND))
+                + " " + (cal.get(Calendar.AM_PM)==0?"AM":"PM")
         );
-
     }
+
+    private String addLeadingZero(int num) {
+        return (num < 10 ? "0" : "") + num;
+    }
+
+    private  String getMonthForInt(int num) {
+        String month = "wrong";
+        DateFormatSymbols dfs = new DateFormatSymbols();
+        String[] months = dfs.getMonths();
+        if (num >= 0 && num <= 11 ) {
+            month = months[num];
+        }
+        return month.substring(0,3);
+    }
+
     public String usingDateFormatter(long input){
         Date date = new Date(input);
         Calendar cal = new GregorianCalendar();
