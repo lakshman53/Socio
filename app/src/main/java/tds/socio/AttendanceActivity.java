74b3dd206dd61d031215533ce087da3ac6e994e1
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-02-03 18:50:41 +0530

    Initial Commit

--- app/src/main/java/tds/socio/AttendanceActivity.java
+++ app/src/main/java/tds/socio/AttendanceActivity.java
@@ -4,13 +4,21 @@ package tds.socio;
  * Created by laks on 29-01-2015.
  */
 
+import android.content.Intent;
 import android.content.res.TypedArray;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import tds.libs.GPSTracker;
 import tds.libs.SntpClient;
@@ -20,6 +28,54 @@ public class AttendanceActivity extends BaseActivity {
     private String[] navMenuTitles;
     private TypedArray navMenuIcons;
     long lngCurrentTime = -1;
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Socio_" + timeStamp + "_";
+
+        //File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File storageDir =  Environment.getExternalStorageDirectory();
+
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+//        Save a file: path for use with ACTION_VIEW intents
+
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +97,17 @@ public class AttendanceActivity extends BaseActivity {
         {
             double latitude = gps.getLatitude();
             double longitude = gps.getLongitude();
-            String StrdistBetCoor = Double.toString(gps.distance(latitude, longitude, 17.3986852, 78.3896163, 'C'));
-            Toast.makeText(getApplicationContext(),StrdistBetCoor,Toast.LENGTH_LONG).show();
+
+//          String StrdistBetCoor = Double.toString(gps.distance(latitude, longitude, 17.3986852, 78.3896163, 'C'));
+//          Toast.makeText(getApplicationContext(),StrdistBetCoor,Toast.LENGTH_LONG).show();
+
+            try
+            {
+                dispatchTakePictureIntent();
+            }
+           catch (Exception e) {
+               Log.e("Capture Image Error: " , e.getMessage());
+           }
         }
         else
         {
