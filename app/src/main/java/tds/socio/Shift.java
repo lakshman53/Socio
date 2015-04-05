e14050e7c13bed9c628af05b5e53ad2c0a5170a9
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-04-05 21:46:16 +0530

    Initial Commit

---
+++ app/src/main/java/tds/socio/Shift.java
@@ -0,0 +1,45 @@
+package tds.socio;
+
+import java.util.Date;
+
+/**
+ * Created by laks on 05-04-2015.
+ */
+
+public class Shift {
+    String shiftName;
+    Date startTime, EndTime;
+
+    public Shift(String shiftName, Date startTime, Date endTime) {
+        this.shiftName = shiftName;
+        this.startTime = startTime;
+        EndTime = endTime;
+    }
+
+    public Shift() {
+    }
+
+    public String getShiftName() {
+        return shiftName;
+    }
+
+    public void setShiftName(String shiftName) {
+        this.shiftName = shiftName;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return EndTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        EndTime = endTime;
+    }
+}
