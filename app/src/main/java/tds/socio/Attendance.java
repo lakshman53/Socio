a806702ffd4dd8b46fafe7536ea61603ff034a57
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-04-05 21:46:37 +0530

    Initial Commit

---
+++ app/src/main/java/tds/socio/Attendance.java
@@ -0,0 +1,48 @@
+package tds.socio;
+
+import com.orm.SugarRecord;
+
+import java.util.Date;
+
+/**
+ * Created by laks on 05-04-2015.
+ */
+public class Attendance extends SugarRecord<Attendance> {
+
+    Date logDateTime;
+    String Flag;
+    Shift shift;
+
+    public Attendance() {    }
+
+    public Attendance(Date logDateTime, String flag, Shift shift) {
+        this.logDateTime = logDateTime;
+        Flag = flag;
+        this.shift = shift;
+    }
+
+    public Date getLogDateTime() {
+        return logDateTime;
+    }
+
+    public void setLogDateTime(Date logDateTime) {
+        this.logDateTime = logDateTime;
+    }
+
+    public String getFlag() {
+        return Flag;
+    }
+
+    public void setFlag(String flag) {
+        Flag = flag;
+    }
+
+    public Shift getShift() {
+        return shift;
+    }
+
+    public void setShift(Shift shift) {
+        this.shift = shift;
+    }
+}
+
