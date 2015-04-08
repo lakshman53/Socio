a72df13ebff1053f9a8b585e68ebadd61070345c
Lakshman Rao Pilaka <lakshman.pilaka@gmail.com>
2015-04-09 00:28:34 +0530

    Initial Commit

---
+++ app/src/main/java/tds/socio/dateTimeClass.java
@@ -0,0 +1,16 @@
+package tds.socio;
+
+/**
+ * Created by laks on 08-04-2015.
+ */
+public class dateTimeClass {
+    public static volatile String[] dateTimeArray = {};
+
+    public String[] getDateTimeArray() {
+        return dateTimeArray;
+    }
+
+    public void setDateTimeArray(String[] dateTimeArray) {
+        this.dateTimeArray = dateTimeArray;
+    }
+}
